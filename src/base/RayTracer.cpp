d0a975f72f3123dcda815580b7ed284861b29a69
jun huang <HYJungao@outlook.com>
2024-02-17 16:47:53 +0800

    code style

--- src/base/RayTracer.cpp
+++ src/base/RayTracer.cpp
@@ -310,6 +310,135 @@ std::unique_ptr<BvhNode> RayTracer::constructBvhSah(size_t start, size_t end) {
     }
 }
 
+std::unique_ptr<BvhNode> RayTracer::constructBvhSahOptimalDim(size_t start, size_t end) {
+
+    std::unique_ptr<BvhNode> node = std::make_unique<BvhNode>();
+
+    if (end - start <= 2) {
+        size_t index = m_indices->at(start);
+        AABB box(m_triangles->at(index).min(), m_triangles->at(index).max());
+        for (size_t i = start + 1; i < end; ++i) {
+            index = m_indices->at(i);
+            box.min = FW::min(box.min, m_triangles->at(index).min());
+            box.max = FW::max(box.max, m_triangles->at(index).max());
+        }
+
+        node->bb = std::move(box);
+        node->startPrim = start;
+        node->endPrim = end;
+        node->left = nullptr;
+        node->right = nullptr;
+        return node;
+    }
+    else
+    {
+        size_t index = m_indices->at(start);
+        AABB box(m_triangles->at(index).centroid(), m_triangles->at(index).centroid());
+        for (size_t i = start + 1; i < end; ++i) {
+            index = m_indices->at(i);
+            box.min = FW::min(box.min, m_triangles->at(index).centroid());
+            box.max = FW::max(box.max, m_triangles->at(index).centroid());
+        }
+
+        Vec3f diagonal = box.max - box.min;
+
+        if (diagonal.x > diagonal.y && diagonal.x > diagonal.z) {
+            std::sort(m_indices->begin() + start, m_indices->begin() + end, [this](auto num1, auto num2) {
+                Vec3f tmp1 = (m_triangles->at(num1).min() + m_triangles->at(num1).max()) * 0.5;
+                Vec3f tmp2 = (m_triangles->at(num2).min() + m_triangles->at(num2).max()) * 0.5;
+                return tmp1.x < tmp2.x;
+                });
+        }
+        else if (diagonal.y > diagonal.z) {
+            std::sort(m_indices->begin() + start, m_indices->begin() + end, [this](auto num1, auto num2) {
+                Vec3f tmp1 = (m_triangles->at(num1).min() + m_triangles->at(num1).max()) * 0.5;
+                Vec3f tmp2 = (m_triangles->at(num2).min() + m_triangles->at(num2).max()) * 0.5;
+                return tmp1.y < tmp2.y;
+                });
+        }
+        else {
+            std::sort(m_indices->begin() + start, m_indices->begin() + end, [this](auto num1, auto num2) {
+                Vec3f tmp1 = (m_triangles->at(num1).min() + m_triangles->at(num1).max()) * 0.5;
+                Vec3f tmp2 = (m_triangles->at(num2).min() + m_triangles->at(num2).max()) * 0.5;
+                return tmp1.z < tmp2.z;
+                });
+        }
+
+        size_t optMid = start + ((end - start) / 2);
+        float minCost = std::numeric_limits<float>::max();
+
+        // ****** unoptimized version of SAH, O(n^2) ******
+        //for (size_t i = start; i < end; ++i) {
+        //    size_t leftIndex= m_indices->at(start);
+        //    size_t rightIndex = m_indices->at(i);
+        //    AABB leftBox(m_triangles->at(leftIndex).centroid(), m_triangles->at(leftIndex).centroid());
+        //    AABB rightBox(m_triangles->at(rightIndex).centroid(), m_triangles->at(rightIndex).centroid());
+
+        //    for (size_t ii = start + 1; ii < i; ++ii) {
+        //        leftIndex = m_indices->at(ii);
+        //        leftBox.min = FW::min(leftBox.min, m_triangles->at(leftIndex).centroid());
+        //        leftBox.max = FW::max(leftBox.max, m_triangles->at(leftIndex).centroid());
+        //    }
+
+        //    for (size_t ii = i + 1; ii < end; ++ii) {
+        //        rightIndex = m_indices->at(ii);
+        //        rightBox.min = FW::min(rightBox.min, m_triangles->at(rightIndex).centroid());
+        //        rightBox.max = FW::max(rightBox.max, m_triangles->at(rightIndex).centroid());
+        //    }
+
+        //    float leftArea = leftBox.area();
+        //    float rightArea = rightBox.area();
+        //    float totalArea = box.area();
+        //    float cost = (leftArea * (i - start) + rightArea * (end - i)) / totalArea;
+        //    if (cost < minCost) {
+        //        minCost = cost;
+        //        optMid = i;
+        //    }
+        //}
+
+
+        // ****** optimized version of SAH, O(n) ******
+        // ****** first compute all AABB area of [start,i] and [i,end], reduce redundant computation ******
+        size_t leftIndex = m_indices->at(start);
+        size_t rightIndex = m_indices->at(end - 1);
+        AABB leftBox(m_triangles->at(leftIndex).centroid(), m_triangles->at(leftIndex).centroid());
+        AABB rightBox(m_triangles->at(rightIndex).centroid(), m_triangles->at(rightIndex).centroid());
+
+        std::vector<float> leftChildrenArea(end - start);
+        std::vector<float> rightChildrenArea(end - start);
+
+        for (size_t i = start; i < end; ++i) {
+            leftIndex = m_indices->at(i);
+            leftBox.min = FW::min(leftBox.min, m_triangles->at(leftIndex).centroid());
+            leftBox.max = FW::max(leftBox.max, m_triangles->at(leftIndex).centroid());
+            leftChildrenArea[i - start] = leftBox.area();
+
+            rightIndex = m_indices->at(start + end - i - 1);
+            rightBox.min = FW::min(rightBox.min, m_triangles->at(rightIndex).centroid());
+            rightBox.max = FW::max(rightBox.max, m_triangles->at(rightIndex).centroid());
+            rightChildrenArea[end - 1 - i] = rightBox.area();
+        }
+
+        float totalArea = box.area();
+        for (size_t i = start; i < end; ++i) {
+            float leftArea = leftChildrenArea[i - start];
+            float rightArea = rightChildrenArea[i - start];
+            float cost = (leftArea * (i - start) + rightArea * (end - i)) / totalArea;
+            if (cost < minCost) {
+                minCost = cost;
+                optMid = i;
+            }
+        }
+
+        node->left = constructBvhSahOptimalDim(start, optMid);
+        node->right = constructBvhSahOptimalDim(optMid, end);
+        node->startPrim = start;
+        node->endPrim = end;
+        node->bb = AABB(FW::min(node->left->bb.min, node->right->bb.min), FW::max(node->left->bb.max, node->right->bb.max));
+        return node;
+    }
+}
+
 void RayTracer::constructHierarchy(std::vector<RTTriangle>& triangles, SplitMode splitMode) {
     // YOUR CODE HERE (R1):
     // This is where you should construct your BVH.
@@ -318,7 +447,18 @@ void RayTracer::constructHierarchy(std::vector<RTTriangle>& triangles, SplitMode
     m_indices = &(m_bvh.getIndices());
     m_indices->resize(triangles.size());
     std::iota(m_indices->begin(), m_indices->end(), 0);
-    std::unique_ptr<BvhNode> root = constructBvhSah(0, triangles.size());
+    std::unique_ptr<BvhNode> root;
+    switch (splitMode) {
+    case SplitMode::SplitMode_ObjectMedian:
+        root = constructBvh(0, triangles.size());
+        break;
+    case SplitMode::SplitMode_Sah:
+        root = constructBvhSah(0, triangles.size());
+        break;
+    default:
+        root = constructBvh(0, triangles.size());
+        break;
+    }
     m_bvh.setRoot(std::move(root));
 }
 
--- src/base/RayTracer.hpp
+++ src/base/RayTracer.hpp
@@ -47,6 +47,7 @@ public:
 private:
     std::unique_ptr<BvhNode> constructBvh(size_t start, size_t end);
     std::unique_ptr<BvhNode> constructBvhSah(size_t start, size_t end);
+    std::unique_ptr<BvhNode> constructBvhSahOptimalDim(size_t start, size_t end);
     RaycastResult intersect(const BvhNode& node, const Vec3f& orig, const Vec3f& dir, const Vec3f& normDir, const Vec3f& invDir) const;
 	mutable std::atomic<int> m_rayCount;
 	Bvh m_bvh;
